b4822cf17d277f80e7d6ce689065a4a2fa013ee0
Kelvin Kiptum Kiprop <kelvin.kiptum@alternateltd.com>
2020-10-29 00:30:32 +0300

    # GENERAL - fine tuned pageTitleSorter function in global.js - updates on general html and styles

--- SiteAssets/js/global.js
+++ SiteAssets/js/global.js
@@ -5,17 +5,8 @@ $(() => {
 function pageTitleSorter() {
 	var url = window.location.href;
 
-	if (url.includes('dashboard')) {
-		$('.page-title').html('dashboard');
-	} else if (url.includes('patients')) {
-		$('.page-title').html('patients')
-	} else if (url.includes('details')) {
-		$('.page-title').html('details')
-	} else if (url.includes('procurement')) {
-		$('.page-title').html('procurement')
-	} else if (url.includes('specialists')) {
-		$('.page-title').html('specialists')
-	} else if (url.includes('notifications')) {
-		$('.page-title').html('notifications')
-	}
+	let splitUrl = url.split('/'),
+		composedUrl = splitUrl[splitUrl.length - 1].split('.')[0];
+	$('.side-nav .list-group').find('.list-group-item:contains(' + composedUrl + ')').addClass('active');
+	$('.page-title').html(b);
 }
