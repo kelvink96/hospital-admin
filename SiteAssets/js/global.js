1dd9bea912e1b7cb0eeff0bcd1ef61f127d3adeb
Kelvin Kiptum Kiprop <kelvin.kiptum@alternateltd.com>
2020-11-07 23:21:03 +0300

    # RESPONSIVENESS - updated navbar, side-nav, base-layout styles version on small devices

--- SiteAssets/js/global.js
+++ SiteAssets/js/global.js
@@ -1,6 +1,16 @@
 $(() => {
 	pageTitleSorter();
+	bsEvents();
 })
+
+function bsEvents() {
+	$(window).resize(function () {
+		var mql = window.matchMedia("(max-width: 600px)")
+		if (mql.matches) {
+			$('[data-toggle="tooltip"]').tooltip('show')
+		}
+	});
+}
 
 function pageTitleSorter() {
 	var url = window.location.href;
